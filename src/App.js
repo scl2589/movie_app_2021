1147219478f9d78a9d3807616f8391a0f899024e
scl2589 <chaelinshin96@gmail.com>
2021-01-24 01:01:45 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
-// 컴포넌트를 가져올 때는 import 한다
-import Potato from "./Potato";
+// function Food(props) {
+//   console.log(props.fav);
+//   return <h1>I like Potato</h1>;
+// }
+
+// ES6 방식
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   // 캄포넌트는 html을 return하는 형태
   return <div>
     <h1>Hello</h1>
-    <Potato/>
+    {/* kimchi라는 value로 fav라는 이름의 property(props)를 줌 */}
+    <Food fav="kimchi"/>
+    <Food fav="ramen"/>
+    <Food fav="salmon"/>
+    <Food fav="salad"/>
+    <Food fav="pasta"/>
   </div>
 }
 
--- src/Potato.js
+++
@@ -1,8 +0,0 @@
-// 컴포넌트를 생성할 때는 매 번 react를 import 해야한다.
-import React from 'react';
-
-function Potato() {
-    return <h3>I love potato</h3>;
-}
-
-export default Potato;
