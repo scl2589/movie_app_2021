0a46c6094e4c53dab58256334a4ec47ac1ef91c6
scl2589 <chaelinshin96@gmail.com>
2021-01-24 01:36:16 +0900

    Protection with Proptypes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,71 @@
 import React from 'react'
-
-// function Food(props) {
-//   console.log(props.fav);
-//   return <h1>I like Potato</h1>;
-// }
+import PropTypes from 'prop-types'
 
 // ES6 방식
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return <div>
     <h2>I like {name}</h2>
-    <img src={picture} />
+    <h4>rating: {rating}/5</h4>
+    <img src={picture} alt={name}/>
   </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  // 이렇게 하면 콘솔에 에러가 뜬다!! rating expect to be a string, but it is in number
+  rating: PropTypes.number.isRequired
 }
 
 // foodILike는 object이다.
 const foodILike = [
   {
+    id:1,
     name: "Kimchi",
     image:
-      "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png"
+      "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
+    rating: 5
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
-      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+    rating: 4.5
   },
   {
     name: "Bibimbap",
     image:
-      "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjI3/MDAxNDkzMDIzMjc4MjU2.L-3Vv9r0XjeRGGncaB0p0II6mw9-NoBfu2k4PMCrTdgg.jP8wA64wrWrXrH3ZTP4UBCPR6ZWppqqnhXkS8FPpYMQg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800"
+      "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjI3/MDAxNDkzMDIzMjc4MjU2.L-3Vv9r0XjeRGGncaB0p0II6mw9-NoBfu2k4PMCrTdgg.jP8wA64wrWrXrH3ZTP4UBCPR6ZWppqqnhXkS8FPpYMQg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800",
+    rating: 3,
   },
   {
+    id: 3,
     name: "Doncasu",
     image:
-      "https://th3.tmon.kr/thumbs/image/ca9/338/813/747a4f277_700x700_95_FIT_1548187062.jpg"
+      "https://th3.tmon.kr/thumbs/image/ca9/338/813/747a4f277_700x700_95_FIT_1548187062.jpg",
+    rating: 4
   },
   {
+    id: 4,
     name: "Kimbap",
     image:
-      "https://static.wtable.co.kr/image/production/service/recipe/689/edd1e01f-4502-453b-8b61-688941aefb76.jpg?size=1024x"
+      "https://static.wtable.co.kr/image/production/service/recipe/689/edd1e01f-4502-453b-8b61-688941aefb76.jpg?size=1024x",
+    rating: 5
   }
 ];
 
 function App() {
   // 캄포넌트는 html을 return하는 형태
   return <div>
-    { foodILike.map(dish => <Food name={dish.name} picture={dish.image} /> )}
+    {foodILike.map(dish => 
+      <Food 
+        key={dish.id}
+        name={dish.name} 
+        picture={dish.image} 
+        rating={dish.rating} 
+      /> 
+    )}
   </div>
 }
 
@@ -63,3 +83,6 @@ export default App;
 //   return friend + "<3";
 // })
 // => 출력하면 ["dal<3", "mark<3", "lynn<3", "japan guy<3"]
+
+// prop-types 설치
+// - prop-types가 할일은 내가 전달받은 props가 내가 원하는 props인지 확인해 준다.
