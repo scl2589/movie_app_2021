2ebbd26286c6483ca230a01914f50e1ae1a55e05
scl2589 <chaelinshin96@gmail.com>
2021-01-24 15:17:57 +0900

    Styling the Movie Service

--- src/Movie.js
+++ src/Movie.js
@@ -7,14 +7,14 @@ function Movie({year, title, summary, poster, genres}) {
         <div className="movie">
             <img src={poster} alt={title} title={title}/>
             <div className="movie__data">
-                <h3 className="movie__title" style={{backgroundColor:"red"}}>{title}</h3>
+                <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
                 <ul className="genres">
                     {genres.map((genre, index) => (
                         <li key={index} className="genres__genre">{genre}</li>
                     ))}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     )
